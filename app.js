d12bfaa5de1c5a65de9839561432e48112368441
brianwlego <legobw@gmail.com>
2020-12-02 16:50:59 -0500

    moving async function

--- app.js
+++ app.js
@@ -18,21 +18,22 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', ()=> {
+    return req.on('end', ()=> {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      res.end();
+      fs.writeFile('message.txt', message, ()=> {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     })
   }
-  // res.setHeader('Content-Type', 'text/html')
-  // res.write('<html>')
-  // res.write('<head><title>First Page</title></head>')
-  // res.write('<body><h1>Hello from my Node.js server!</h1></body>')
-  // res.write('</html>')
-  // res.end();
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>First Page</title></head>')
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>')
+  res.write('</html>')
+  res.end();
 });
 
 server.listen(3000);
