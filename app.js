fc145f7b3066d0364accdabb59680db4ae0eb86e
brianwlego <legobw@gmail.com>
2020-12-05 21:20:57 -0500

    Changes to express

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("in the middleware")
+  next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("in the middleware #2")
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
 
