0bad75d06389c5f51ba954d81affc4e36f484f11
brianwlego <legobw@gmail.com>
2020-12-09 13:55:08 -0500

    Added Models to app

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,4 @@
-const products = [];
-
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -11,16 +10,17 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({title: req.body.title})
+  const product = new Product(req.body.title)
+  product.save();
   res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
-  // res.sendFile(path.join(rootDir,  'views', 'shop.html'));
-  
-  res.render('shop', {
-    prods: products, 
-    docTitle: 'Shop', 
-    path: '/', 
+  Product.fetchAll((products)=> {
+    res.render('shop', {
+      prods: products, 
+      docTitle: 'Shop', 
+      path: '/', 
+    });
   });
 }
