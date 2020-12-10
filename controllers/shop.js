d690d060904e83ffbd84d242b97c084e4072c63f
brianwlego <legobw@gmail.com>
2020-12-10 09:14:15 -0500

    adjusting routes and controllers

--- controllers/shop.js
+++ controllers/shop.js
@@ -30,3 +30,9 @@ exports.getCheckout = (req, res, next) => {
     path: '/checkout'
   })
 }
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    pageTitle: 'Orders',
+    path: '/orders'
+  })
+}
